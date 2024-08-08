e86e772246b7cdccc625f23b08a289291d2a5d84
Jekeun Woo <jkeunw@gmail.com>
2024-08-08 11:36:04 +0900

    Update package.json

--- truffle.js
+++ truffle.js
@@ -2,7 +2,7 @@ const PrivateKeyConnector = require('connect-privkey-to-provider')
 const NETWORK_ID = '1001'
 const GASLIMIT = '20000000'
 const URL = `https://api.baobab.klaytn.net:8651`
-const PRIVATE_KEY = '0x40f2f9d44820a5615e3481dbb0cf8cbc561b53f0a5ae7cf1111a4c26b1d488dc'
+const PRIVATE_KEY = ''
 
 module.exports = {
   networks: {  
